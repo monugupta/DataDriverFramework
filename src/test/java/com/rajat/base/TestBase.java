9fd7d6056b3f7c2a8c75f1b0bf50d21a6dc85f11
rajat.gupta2 <Rajat.Gupta2@MBRLT00401.timesgroup.com>
2020-02-07 10:35:12 +0530

    dgdfgfdgfg

--- src/test/java/com/rajat/base/TestBase.java
+++ src/test/java/com/rajat/base/TestBase.java
@@ -36,7 +36,7 @@ import com.rajat.utilites.TestUtil;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 //Interagation og CI CD ass well in this project
-//using git plugin also but fails
+//using git plugin also but fails test
 public class TestBase {
 	public static WebDriver driver;
 	public static String browser;
